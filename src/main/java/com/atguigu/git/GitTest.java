cd68075a2d592f515679ee4abd9c32880d1540d4
LJC <1356382196@qq.com>
2022-11-23 17:51:17 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author LiJiangCheng
+ * @date 2022/11/23 17:47
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
