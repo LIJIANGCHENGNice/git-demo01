0fc0bec9d26f50fb6bb627d966f01cb4db9f07d3
LJC <1356382196@qq.com>
2022-11-23 17:59:59 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -1,12 +1,12 @@
 package com.atguigu.git;
 
-/**
- * @author LiJiangCheng
- * @date 2022/11/23 17:47
- */
+
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello world");
+
+
+        System.out.println("zhangheng");
     }
 }
